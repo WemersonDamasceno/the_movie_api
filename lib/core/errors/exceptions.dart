6c51736a2a6331e88574e7bd7716b23679b429d1
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-25 13:00:02 -0300

    feat: create file exceptions and failures

---
+++ lib/core/errors/exceptions.dart
@@ -0,0 +1,11 @@
+class ServerException implements Exception {
+  const ServerException();
+}
+
+class NotFoundException implements Exception {
+  const NotFoundException();
+}
+
+class ExceptionGenerics implements Exception {
+  const ExceptionGenerics();
+}
