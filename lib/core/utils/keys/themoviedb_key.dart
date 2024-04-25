efbdff972f5de75b4ef488e42aa32dfd441711cf
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-25 17:52:09 -0300

    feat: add api_key to test

--- lib/core/utils/keys/themoviedb_key.dart
+++ lib/core/utils/keys/themoviedb_key.dart
@@ -1,4 +1,4 @@
 class ThemovieDBKey {
-  static const String apiKey = 'API_KEY_HERE';
+  static const String apiKey = '6ee08abf792ae460668806133c782b4c';
   static const String baseUrlImage = 'https://image.tmdb.org/t/p/w500';
 }
