a2571b05b0333c9a050cd3f3cb11d7f992410251
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-25 13:02:10 -0300

    feat: create file icons

---
+++ lib/core/utils/icons/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const String logoTmdb = 'lib/core/assets/images/tmdb_logo.JPEG';
+  static const String error = 'lib/core/assets/animation/error.json';
+  static const String backdrop = 'lib/core/assets/images/back_drop.png';
+  static const String anime = 'lib/core/assets/images/anime.png';
+  static const String movie = 'lib/core/assets/images/movie.png';
+}
