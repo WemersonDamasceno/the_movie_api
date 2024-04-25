a4286d918ca372ce34fb3992b24215bf7f6dd6d2
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-25 13:09:03 -0300

    feat: create main file

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:the_movies_db/core/utils/constants/app_colors.dart';
+import 'package:the_movies_db/injection_container.dart';
+import 'package:the_movies_db/presentation/router/router.dart';
+
+void main() async {
+  await init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'The Movies DB',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "SourceSansPro",
+        primaryColor: AppColors.tmdbDarkBlue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: AppColors.tmdbDarkBlue,
+          secondary: AppColors.tmdbLighterGreen,
+        ),
+      ),
+      routerConfig: router,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale("pt", "BR"),
+      ],
+    );
+  }
+}
