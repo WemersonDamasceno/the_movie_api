419b962fb8e1e53abc1961abb8f5998b7bb88d66
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-25 13:12:22 -0300

    test: create test for entities

---
+++ test/domain/entities/movie_entity_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import 'package:the_movies_db/domain/entities/movie_entity.dart';
+
+import '../../mocks/movie_mock.dart';
+
+void main() {
+  final MovieEntity movie = MovieMock.entity.copyWith();
+
+  test('should return true', () {
+    expect(movie.props.isNotEmpty, true);
+  });
+
+  test('should check consistency of props', () {
+    expect(movie.id, 1);
+  });
+}
