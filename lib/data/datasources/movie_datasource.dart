52f3245eba9b6611d124b029923c183d8621a93f
WemersonDamasceno <wemersondamasceno7@gmail.com>
2024-04-25 13:04:01 -0300

    feat: create datasource classes abstract and implementation

---
+++ lib/data/datasources/movie_datasource.dart
@@ -0,0 +1,14 @@
+import 'package:the_movies_db/data/models/movie_detail_model.dart';
+import 'package:the_movies_db/data/models/movie_model.dart';
+
+abstract class MovieDatasource {
+  Future<List<MovieModel>> getPopularMovies();
+  Future<List<MovieModel>> getTrendingMovies();
+
+  Future<MovieDetailModel> getMovieDetail(int id);
+
+  Future<List<MovieModel>> searchMovies(String query);
+
+  Future<List<MovieModel>> getAllSeries();
+  Future<List<MovieModel>> getAllMovies();
+}
---
+++ lib/data/datasources/themoviedb_datasource_imp.dart
@@ -0,0 +1,139 @@
+import 'dart:convert';
+
+import 'package:the_movies_db/core/errors/exceptions.dart';
+import 'package:the_movies_db/core/http_client/http_client.dart';
+import 'package:the_movies_db/core/utils/constants/themoviedb_endpoints.dart';
+import 'package:the_movies_db/core/utils/keys/themoviedb_key.dart';
+import 'package:the_movies_db/data/datasources/movie_datasource.dart';
+import 'package:the_movies_db/data/models/movie_detail_model.dart';
+import 'package:the_movies_db/data/models/movie_model.dart';
+
+class TheMoviedbDatasourceImp implements MovieDatasource {
+  final HttpClient client;
+  int currentPage = 1;
+  List<MovieModel> listPopularMovies = [];
+
+  TheMoviedbDatasourceImp({required this.client});
+
+  @override
+  Future<List<MovieModel>> getPopularMovies() async {
+    final response = await client.get(
+      TheMoviedbEndpoints.popularMovies(ThemovieDBKey.apiKey, currentPage),
+    );
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.data);
+      for (var movie in json['results']) {
+        listPopularMovies.add(MovieModel.movieFromJson(movie));
+      }
+      currentPage++;
+      return listPopularMovies;
+    } else if (response.statusCode == 503) {
+      throw const ServerException();
+    } else if (response.statusCode == 404) {
+      throw const NotFoundException();
+    } else {
+      throw const ExceptionGenerics();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getTrendingMovies() async {
+    final response = await client.get(
+      TheMoviedbEndpoints.trendingMovies(ThemovieDBKey.apiKey),
+    );
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.data);
+      List<MovieModel> listTrendingMovies = [];
+      listTrendingMovies = json['results']
+          .map<MovieModel>((movie) => MovieModel.movieFromJson(movie))
+          .toList();
+      return listTrendingMovies;
+    } else if (response.statusCode == 503) {
+      throw const ServerException();
+    } else if (response.statusCode == 404) {
+      throw const NotFoundException();
+    } else {
+      throw const ExceptionGenerics();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getAllSeries() async {
+    final List<MovieModel> listAllSeries = [];
+    final response = await client.get(
+      TheMoviedbEndpoints.getAllSeries(ThemovieDBKey.apiKey),
+    );
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.data);
+      for (var movie in json['results']) {
+        listAllSeries.add(MovieModel.seriesFromJson(movie));
+      }
+      return listAllSeries;
+    } else if (response.statusCode == 503) {
+      throw const ServerException();
+    } else if (response.statusCode == 404) {
+      throw const NotFoundException();
+    } else {
+      throw const ExceptionGenerics();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> getAllMovies() async {
+    final List<MovieModel> listAllMovies = [];
+    final response = await client.get(
+      TheMoviedbEndpoints.getAllMovies(ThemovieDBKey.apiKey),
+    );
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.data);
+      for (var movie in json['results']) {
+        listAllMovies.add(MovieModel.movieFromJson(movie));
+      }
+      return listAllMovies;
+    } else if (response.statusCode == 503) {
+      throw const ServerException();
+    } else if (response.statusCode == 404) {
+      throw const NotFoundException();
+    } else {
+      throw const ExceptionGenerics();
+    }
+  }
+
+  @override
+  Future<MovieDetailModel> getMovieDetail(int id) async {
+    final response = await client.get(
+      TheMoviedbEndpoints.movieDetail(ThemovieDBKey.apiKey, id),
+    );
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.data);
+      return MovieDetailModel.fromJson(json);
+    } else if (response.statusCode == 503) {
+      throw const ServerException();
+    } else if (response.statusCode == 404) {
+      throw const NotFoundException();
+    } else {
+      throw const ExceptionGenerics();
+    }
+  }
+
+  @override
+  Future<List<MovieModel>> searchMovies(String query) async {
+    final response = await client.get(
+      TheMoviedbEndpoints.searchMovies(ThemovieDBKey.apiKey, query),
+    );
+    if (response.statusCode == 200) {
+      var json = jsonDecode(response.data);
+      List<MovieModel> listMovies = [];
+      listMovies = json['results']
+          .map<MovieModel>((movie) => MovieModel.movieFromJson(movie))
+          .toList();
+      return listMovies;
+    } else if (response.statusCode == 503) {
+      throw const ServerException();
+    } else if (response.statusCode == 404) {
+      throw const NotFoundException();
+    } else {
+      throw const ExceptionGenerics();
+    }
+  }
+}
