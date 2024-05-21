227f17a49e5e07c900a47ff81fac965b118bed12
Wemerson Damasceno <wemersondamasceno7@gmail.com>
2024-05-21 09:31:49 -0300

    feat: add new image

--- lib/core/utils/icons/app_images.dart
+++ lib/core/utils/icons/app_images.dart
@@ -1,6 +1,6 @@
 class AppImages {
   static const String logoTmdb = 'lib/core/assets/images/tmdb_logo.JPEG';
-  static const String error = 'lib/core/assets/animation/error.json';
+  static const String empty = 'lib/core/assets/images/empty.png';
   static const String backdrop = 'lib/core/assets/images/back_drop.png';
   static const String anime = 'lib/core/assets/images/anime.png';
   static const String movie = 'lib/core/assets/images/movie.png';
